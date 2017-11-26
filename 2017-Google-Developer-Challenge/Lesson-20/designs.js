a70d54071ad9608feaef6d5c4a20cf01c0d1ff1c
Jaime Andres Catano <jaimeandrescatano@gmail.com>
2017-11-26 08:14:43 +0100

    Updated

--- 2017-Google-Developer-Challenge/Lesson-20/designs.js
+++ 2017-Google-Developer-Challenge/Lesson-20/designs.js
@@ -1,20 +1,20 @@
 /**
-   @fileOverview <strong>Main javascript program </strong>
-   @author <a href="mailto:jaimeandrescatano@gmail.com">Jaime Andres Catano</a>
-   @version 1.0
+    @fileOverview <strong>Main javascript program </strong>
+    @author <a href='mailto:jaimeandrescatano@gmail.com'>Jaime Andres Catano</a>
+    @version 1.0
 */
 
 /**
- * @description Table generating function
- * @param  evt
- * @returns  nothing
+    @description Table generating function
+    @param  evt
+    @returns  nothing
  */
 
 function makeGrid (evt) {
     /* Define variables */
     var tableHeight;
     var tableWidth;
-    var tableRow = "";
+    var tableRow = '';
     var i = 1;
 
     /* Prevent to delete the new table */
@@ -27,36 +27,35 @@ function makeGrid (evt) {
           table rows.
     */
     while (true) {
-        $("#pixel_canvas").find("tr").remove();
+        $('#pixel_canvas').find('tr').remove();
         break;
     }
 
     /* Getting the width and height values from the form */
-    tableHeight = $("#input_height").val();
-    tableWidth = $("#input_width").val();
+    tableHeight = $('#input_height').val();
+    tableWidth = $('#input_width').val();
 
     /* Generation of the table */
-    /* -- Using "for" loop in order to produce the <td> */
+    /* -- Using 'for' loop in order to produce the <td> */
     for (var x = 1; x <= tableWidth; x++) {
-        tableRow = tableRow+"<td></td>";
+        tableRow = tableRow+'<td></td>';
     }
 
-    /* -- Using "while" loop in order to produce the <tr> */
+    /* -- Using 'while' loop in order to produce the <tr> */
     while (i <= tableHeight) {
-        $("#pixel_canvas").append("<tr>"+tableRow+"</tr>");
+        $('#pixel_canvas').append('<tr>'+tableRow+'</tr>');
         i = i+1;
     }
-
     return;
 }
 
 /** @description  Getting the selected color
-* @param  evt
-* @returns  nothing
+    @param  evt
+    @returns  nothing
 */
 function chooseColor (evt) {
     /* Color value from color picker */
-    chosedColor = $("#colorPicker").val();
+    chosedColor = $('#colorPicker').val();
 
     /* Asign the color to the cell */
     $('#pixel_canvas').on('click', 'td', function() {
@@ -66,17 +65,19 @@ function chooseColor (evt) {
 }
 
 /** @description  Starting function
-* @param  nothing
-* @returns  nothing
+    @param  nothing
+    @returns  nothing
 */
 function startProgram () {
-    /* makeGrid function activated when the user uses the "submit button"*/
-    $("#sizePicker").on("submit", makeGrid);
+    /* makeGrid function activated when the user uses the 'submit button'*/
+    $('#sizePicker').on('submit', makeGrid);
 
     /* Color picker function */
-    $("#colorPicker").on("change", chooseColor);
+    $('#colorPicker').on('change', chooseColor);
     return;
 }
 
-/** @description  Set the startProgram as the initial function of jQuery object */
+/** @description  Set the startProgram as the initial function of jQuery object
+    TODO Request feedback from classmates and mentors
+*/
 $(startProgram);
